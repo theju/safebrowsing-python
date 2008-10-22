a924dd72f49ca4137ab2d616948e6ee319c8fa98
thejaswi.puthraya <thejaswi.puthraya@b5ec1141-c343-0410-b502-93835ceaeffe>
2008-10-22 04:53:13 +0000

    Refactored prepare_db.py to remove lots of redundant code
    git-svn-id: https://safebrowsing-python.googlecode.com/svn/trunk@18 b5ec1141-c343-0410-b502-93835ceaeffe

--- hash_fetch/prepare_db.py
+++ hash_fetch/prepare_db.py
@@ -16,15 +16,15 @@ class Google_Blacklist(object):
         The constructor initializes the module.
         """
         badware_dict = {"malware": "M","black": "B"}
+        self.key = key
+        self.dbname = dbname
+        self.url = url
+        self.badware_type = badware_type
         try:
-            self.key = key
-            self.dbname = dbname
-            self.url = url
-            self.badware_type = badware_type
-            try:
-                self.badware_code = badware_dict[badware_type]
-            except KeyError:
-                raise KeyError("Invalid Badware Type")
+            self.badware_code = badware_dict[badware_type]
+        except KeyError:
+            raise KeyError("Invalid Badware Type")
+        try:
             assert self.key
             assert self.dbname
             assert self.url
@@ -38,48 +38,46 @@ class Google_Blacklist(object):
                 cur = conn.cursor()
                 cur.execute("select * from %s_version;" %(self.badware_type))
                 row = cur.fetchall()
-                s = string.Template(self.url)
-                try:
-                    assert row
-                    self.version_number = row[0][0]
-                    self.final_url = s.safe_substitute(key = self.key,\
-                                                       badware_type = self.badware_type,\
-                                                       version = self.version_number)
-                    self.fetch_url_pointer = urllib2.urlopen(self.final_url)
-                    self.url_hashes_data = self.fetch_url_pointer.readlines()
-                    if self.url_hashes_data == []:
-                        return 0
-                    for url_hash in self.url_hashes_data[1:-1]:
-                        if re.match("^-\w+", url_hash):
-                            cur.execute("delete from url_hashes_table where badware_type='%s' and url_hash='%s';" %(self.badware_code, url_hash[1:].strip()))
-                            del self.url_hashes_data[self.url_hashes_data.index(url_hash)]
-                    new_version_number = ":".join(re.compile("\d\.\d+").search(self.url_hashes_data[0]).group().split("."))
-                    cur.execute("update %s_version set version_number='%s' where version_number='%s';" %(self.badware_type, new_version_number, self.version_number))
-                    self.version_number = new_version_number
-                    for url_hash in self.url_hashes_data[1:]:
-                        if not url_hash == '\n':
-                            cur.execute("insert into url_hashes_table (badware_type,url_hash) values ('%s','%s');" %(self.badware_code, url_hash[1:].strip()))
-                except AssertionError:
-                    # Start from Version 1:-1
-                    self.version_number = "1:-1"
-                    self.final_url = s.safe_substitute(key = self.key, badware_type = self.badware_type, version = self.version_number)
-                    self.fetch_url_pointer = urllib2.urlopen(self.final_url)
-                    self.url_hashes_data = self.fetch_url_pointer.readlines()
-                    if self.url_hashes_data == []:
-                        # No data, so no point checking version 
-                        # number. This case might be because of
-                        # throttling or no updates available.
-                        return 0
-                    new_version_number = ":".join(re.compile("\d\.\d+").search(self.url_hashes_data[0]).group().split("."))
-                    self.version_number = new_version_number                   
-                    cur.execute("insert into %s_version (version_number) values ('%s');" %(self.badware_type, self.version_number))
-                    for url_hash in self.url_hashes_data[1:]:
-                        if not url_hash == '\n':
-                            cur.execute("insert into url_hashes_table (badware_type,url_hash) values ('%s','%s');" %(self.badware_code, url_hash[1:].strip()))
-                cur.close()
-                conn.commit()
-                conn.close()
-                return 0
             except sqlite.DatabaseError:
-                raise sqlite.DatabaseError("Error in a Database Specific Operation.")
+                raise sqlite.DatabaseError("Error in a database specific operation.")
+            # TODO: Python 2.5 only, Python 2.4???
+            s = string.Template(self.url)
+            try:
+                assert row
+                self.version_number = row[0][0]
+            except AssertionError:
+                # Start from Version 1:-1
+                self.version_number = "1:-1"
+            self.final_url = s.safe_substitute(key = self.key,
+                                               badware_type = self.badware_type,
+                                               version = self.version_number)
+            self.fetch_url_pointer = urllib2.urlopen(self.final_url)
+            self.url_hashes_data = self.fetch_url_pointer.readlines()
+            if self.url_hashes_data == []:
+                # No data, so no point checking version 
+                # number. This case might be because of
+                # throttling or no updates available.
+                return 0
+            for url_hash in self.url_hashes_data[1:-1]:
+                if re.match("^-\w+", url_hash):
+                    cur.execute("delete from url_hashes_table where "
+                                "badware_type='%s' and url_hash='%s';" %(self.badware_code, url_hash[1:].strip()))
+                    del self.url_hashes_data[self.url_hashes_data.index(url_hash)]
+            new_version_number = ":".join(re.compile("\d\.\d+").search(self.url_hashes_data[0]).group().split("."))
+            if self.version_number == "1:-1":
+                self.version_number = new_version_number
+                cur.execute("insert into %s_version (version_number) "
+                            "values ('%s');" %(self.badware_type, self.version_number))
+            else:
+                cur.execute("update %s_version set version_number='%s' "
+                            "where version_number='%s';" %(self.badware_type, 
+                                                           new_version_number, 
+                                                           self.version_number))
+            for url_hash in self.url_hashes_data[1:]:
+                if not url_hash == '\n':
+                    cur.execute("insert into url_hashes_table (badware_type,url_hash) values ('%s','%s');" %(self.badware_code, url_hash[1:].strip()))
+            cur.close()
+            conn.commit()
+            conn.close()
+            return 0
                                                             
