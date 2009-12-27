29af2af622160a136711e55e8f2240fe4cde8264
thejaswi.puthraya@gmail.com <thejaswi.puthraya@gmail.com@b5ec1141-c343-0410-b502-93835ceaeffe>
2009-12-27 10:12:18 +0000

    Added a tests file
    Conflicts:
    
    	safebrowsing/tests.py
    
    git-svn-id: https://safebrowsing-python.googlecode.com/svn/trunk@42 b5ec1141-c343-0410-b502-93835ceaeffe

--- safebrowsing/tests.py
+++ safebrowsing/tests.py
@@ -80,5 +80,76 @@ class SafebrowsingTestCase(unittest.TestCase):
         self.assertEqual(result, u'M')
 
 
+=======
+        lookup_list = ['f.g/1.html?param=1#Tag',
+                       'f.g/1.html?param=1',
+                       'f.g/1.html',
+                       'f.g/',
+                       'e.f.g/1.html?param=1#Tag',
+                       'e.f.g/1.html?param=1',
+                       'e.f.g/1.html',
+                       'e.f.g/',
+                       'd.e.f.g/1.html?param=1#Tag',
+                       'd.e.f.g/1.html?param=1',
+                       'd.e.f.g/1.html',
+                       'd.e.f.g/',
+                       'c.d.e.f.g/1.html?param=1#Tag',
+                       'c.d.e.f.g/1.html?param=1',
+                       'c.d.e.f.g/1.html',
+                       'c.d.e.f.g/',
+                       'b.c.e.f.g/1.html?param=1#Tag',
+                       'b.c.e.f.g/1.html?param=1',
+                       'b.c.e.f.g/1.html',
+                       'b.c.e.f.g/',
+                       'a.b.c.e.f.g/1.html?param=1#Tag',
+                       'a.b.c.e.f.g/1.html?param=1',
+                       'a.b.c.e.f.g/1.html',
+                       'a.b.c.e.f.g/']
+        self.assertEqual(self.lookup_obj.lookup_list, lookup_list)
+
+    def testLookupList2(self):
+        self.lookup_obj.lookup_by_url('http://a.b.c.d.e.f.g/1/2.html?param=1#Tag')
+        lookup_list = ['f.g/1/2.html?param=1#Tag',
+                       'f.g/1/2.html?param=1',
+                       'f.g/1/2.html',
+                       'f.g/1/',
+                       'f.g/',
+                       'e.f.g/1/2.html?param=1#Tag',
+                       'e.f.g/1/2.html?param=1',
+                       'e.f.g/1/2.html',
+                       'e.f.g/1/',
+                       'e.f.g/',
+                       'd.e.f.g/1/2.html?param=1#Tag',
+                       'd.e.f.g/1/2.html?param=1',
+                       'd.e.f.g/1/2.html',
+                       'd.e.f.g/1/',
+                       'd.e.f.g/',
+                       'c.d.e.f.g/1/2.html?param=1#Tag',
+                       'c.d.e.f.g/1/2.html?param=1',
+                       'c.d.e.f.g/1/2.html',
+                       'c.d.e.f.g/1/',
+                       'c.d.e.f.g/',
+                       'b.c.d.e.f.g/1/2.html?param=1#Tag',
+                       'b.c.d.e.f.g/1/2.html?param=1',
+                       'b.c.d.e.f.g/1/2.html',
+                       'b.c.d.e.f.g/1/',
+                       'b.c.d.e.f.g/',
+                       'a.b.c.d.e.f.g/1/2.html?param=1#Tag',
+                       'a.b.c.d.e.f.g/1/2.html?param=1',
+                       'a.b.c.d.e.f.g/1/2.html',
+                       'a.b.c.d.e.f.g/1/',
+                       'a.b.c.d.e.f.g/',
+                       'a.b.c.d.e.f.g/1/2.html?param=1#Tag',
+                       'a.b.c.d.e.f.g/1/2.html?param=1',
+                       'a.b.c.d.e.f.g/1/2.html',
+                       'a.b.c.d.e.f.g/1/',
+                       'a.b.c.d.e.f.g/',
+                       'a.b.c.d.e.f.g/1/2.html?param=1#Tag',
+                       'a.b.c.d.e.f.g/1/2.html?param=1',
+                       'a.b.c.d.e.f.g/1/2.html',
+                       'a.b.c.d.e.f.g/1/',
+                       'a.b.c.d.e.f.g/',]
+        self.assertEqual(self.lookup_obj.lookup_list, lookup_list)
+
 if __name__ == '__main__':
     unittest.main()
