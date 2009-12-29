90644d5f3d35f73505685cc0828e514b43dc65b9
thejaswi.puthraya@gmail.com <thejaswi.puthraya@gmail.com@b5ec1141-c343-0410-b502-93835ceaeffe>
2009-12-29 16:08:49 +0000

    Added the base DBObj to the base.py file
    git-svn-id: https://safebrowsing-python.googlecode.com/svn/trunk@50 b5ec1141-c343-0410-b502-93835ceaeffe

---
+++ safebrowsing/base.py
@@ -0,0 +1,35 @@
+from platform import node
+import conf
+
+class BaseDbObj(object):
+    db_engine   = getattr(conf, 'DATABASE_ENGINE')
+    db_name     = getattr(conf, 'DATABASE_NAME')
+    db_user     = getattr(conf, 'DATABASE_USER')
+    db_password = getattr(conf, 'DATABASE_PASSWORD')
+    db_host     = getattr(conf, 'DATABASE_HOST', node())
+    db_port     = getattr(conf, 'DATABASE_PORT')
+    api_key     = getattr(conf, 'API_KEY')
+
+    def get_version(self, badware_type):
+        """To be subclassed by backends"""
+        return NotImplementedError
+
+    def insert_version_row(self, badware_type, version_number):
+        """To be subclassed by backends"""
+        return NotImplementedError
+
+    def update_version_row(self, badware_type, new_version_number, version_number):
+        """To be subclassed by backends"""
+        return NotImplementedError
+
+    def insert_row(self, badware_code, url_hash):
+        """To be subclassed by backends"""
+        return NotImplementedError
+
+    def delete_row(self, badware_code, url_hash):
+        """To be subclassed by backends"""
+        return NotImplementedError
+
+    def lookup_by_md5(self, md5_hash):
+        """To be subclassed by backends"""
+        return NotImplementedError
